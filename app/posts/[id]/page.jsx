4ea8271d7c85446d5d599887a5d91300a188637f
Richard <ricky201325@gmail.com>
2025-06-19 13:55:51 -0300

    params

---
+++ app/posts/[id]/page.jsx
@@ -0,0 +1,20 @@
+async function loadPost(id) {
+  const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`)
+  const data = await res.json()
+  console.log(data)
+  return data
+}
+
+async function Page({params}) {
+  const post = await loadPost(params.id) // Cambiar a params.id
+
+  return (
+    <div>
+      <h1>Post Page {params.id}</h1> {/* Cambiar a params.id */}
+      <h2>{post.title}</h2>
+      <p>{post.body}</p>
+    </div>
+  )
+}
+
+export default Page
---
+++ app/posts/page.jsx
@@ -0,0 +1,20 @@
+import PostCard from "@/components/PostCard";
+async function loadPosts() {
+  const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+  const data = await res.json();
+  console.log(data);
+
+  // await new Promise((resolve) => setTimeout(resolve, 4000)); // Simula un retraso de 2 segundos
+  return data; // Agrega esta línea
+}
+async function PostPages() {
+  const posts = await loadPosts();
+  return (
+    <div>
+      {posts.map((post) => (
+        <PostCard key={post.id} post={post} />
+      ))}
+    </div>
+  );
+}
+export default PostPages;
