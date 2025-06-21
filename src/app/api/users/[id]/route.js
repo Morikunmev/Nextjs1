970a8438a4f7dd9cb67fd2f60e33f4bffcf4796a
Richard <ricky201325@gmail.com>
2025-06-20 23:46:32 -0300

    search params

--- src/app/api/users/[id]/route.js
+++ src/app/api/users/[id]/route.js
@@ -1,7 +1,13 @@
-import { NextResponse } from 'next/server';
+import { NextResponse } from "next/server";
 
-export async function GET(request, {params}) {
-   const res = await fetch(`https://jsonplaceholder.typicode.com/users/${params.id}`)
-   const data = await res.json()
-   return NextResponse.json(data)
+export async function GET(request, { params }) {
+  const { searchParams } = new URL(request.url);
+  console.log(searchParams);
+  console.log(searchParams.get("nombre"));
+  console.log(searchParams.get("apellido"));
+  const res = await fetch(
+    `https://jsonplaceholder.typicode.com/users/${params.id}`
+  );
+  const data = await res.json();
+  return NextResponse.json(data);
 }
